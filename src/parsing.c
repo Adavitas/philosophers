1369559fc380b3fa7220fbb964fe8a9c4acbedcd
Nobunaga-Z <leqso.davitashvili.st@gmail.com>
2026-02-12 21:26:43 +0100

    removed from valid_num extra argument and made code cleaner

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: adavitas <adavitas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 18:20:01 by adavitas          #+#    #+#             */
-/*   Updated: 2026/02/12 17:46:49 by adavitas         ###   ########.fr       */
+/*   Updated: 2026/02/12 21:22:24 by adavitas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static int	is_numeric(char *str)
 	return (1);
 }
 
-static int	is_valid_num(char *str, int max)
+static int	is_valid_num(char *str)
 {
 	int	val;
 
@@ -37,24 +37,25 @@ static int	is_valid_num(char *str, int max)
 	val = ft_atoi(str);
 	if (val <= 0)
 		return (0);
-	if (max && val > MAX_PHILOS)
-		return (0);
 	return (1);
 }
 
 int	validate_input(int argc, char **argv)
 {
+	int num_philos;
+
 	if (argc < 5 || argc > 6)
 		return (printf("Usage: ./philo 5 800 200 200 [meals]\n"));
-	if (!is_valid_num(argv[1], 1))
+	num_philos = ft_atoi(argv[1]);
+	if (!is_valid_num(argv[1]) || num_philos > MAX_PHILOS)
 		return (printf("Error: invalid number_of_philosophers\n"));
-	if (!is_valid_num(argv[2], 0))
+	if (!is_valid_num(argv[2]))
 		return (printf("Error: invalid time_to_die\n"));
-	if (!is_valid_num(argv[3], 0))
+	if (!is_valid_num(argv[3]))
 		return (printf("Error: invalid time_to_eat\n"));
-	if (!is_valid_num(argv[4], 0))
+	if (!is_valid_num(argv[4]))
 		return (printf("Error: invalid time_to_sleep\n"));
-	if (argc == 6 && !is_valid_num(argv[5], 0))
+	if (argc == 6 && !is_valid_num(argv[5]))
 		return (printf("Error: invalid number_of_meals\n"));
 	return (0);
 }
